62329c0b080492ac1e87626bf5fc9bc8f331fc9c
lojoeugenio4-max <lojoeugenio4@gmail.com>
2026-05-05 16:50:29 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// 👉 Esto evita el zoom en móvil
-let viewport = document.querySelector("meta[name=viewport]");
-
-if (!viewport) {
-  viewport = document.createElement("meta");
-  viewport.setAttribute("name", "viewport");
-  document.head.appendChild(viewport);
-}
-
-viewport.setAttribute(
-  "content",
-  "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-createRoot(document.getElementById("root")).render(<App />);import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
